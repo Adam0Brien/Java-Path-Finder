88d97f7be30c2940faf7953463401a4bf3227f3c
Corey <coreyshanahan02@gmail.com>
2022-04-05 13:39:24 +0100

    Added the 3 room lists

--- src/main/java/controller/GalleryAPI.java
+++ src/main/java/controller/GalleryAPI.java
@@ -1,4 +1,22 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Room;
+import utils.GraphNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class GalleryAPI {
+
+    private List<Room> rooms;
+    private ObservableList<String> names;
+    private List<GraphNode<Room>> roomNodes;
+
+    public GalleryAPI(){
+        this.rooms = new LinkedList<>();
+        this.names = FXCollections.observableArrayList();
+        this. roomNodes = new LinkedList<>();
+    }
 }
