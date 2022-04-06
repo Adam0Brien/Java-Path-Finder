42c0c249bf48f1a3e8b7d3071b6331a6c252a409
Corey <coreyshanahan02@gmail.com>
2022-04-06 13:19:05 +0100

    Added icon

--- src/main/java/main/Driver.java
+++ src/main/java/main/Driver.java
@@ -3,6 +3,7 @@ package main;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,9 @@ public class Driver extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FXML/gallery.fxml"));
+        Image icon = new Image(getClass().getResourceAsStream("/Images/MSPaint.png"));
         Scene scene = new Scene(fxmlLoader.load(), 925, 614);
+        stage.getIcons().add(icon);
         stage.setTitle("National Gallery Route Finder");
         stage.setScene(scene);
         stage.show();
