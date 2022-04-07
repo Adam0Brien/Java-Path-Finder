8fde0e1e1a4da68836532ea26afc6f060ac44780
Corey <coreyshanahan02@gmail.com>
2022-04-07 16:44:00 +0100

    Created GalleryAPITest.java

---
+++ src/test/java/controller/GalleryAPITest.java
@@ -0,0 +1,31 @@
+package controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GalleryAPITest {
+
+    GalleryAPI galleryAPI;
+
+    @BeforeEach
+    void setUp() {
+        galleryAPI = new GalleryAPI();
+    }
+
+    @AfterEach
+    void tearDown() {
+        galleryAPI = null;
+    }
+
+    @Test
+    void findShortestRoute() {
+    }
+
+    @Test
+    void generateMultipleRoutes() {
+
+    }
+}
